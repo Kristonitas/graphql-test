48bc5e1e0130a6c062b606e3f896aa4eace138ed
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2020-01-01 16:03:55 +0200

    Advaced queries

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
 import express from "express";
 import { createServer } from "http";
-import expressGraphQL from "express-graphql";
-import { buildSchema } from "graphql";
+import graph from "./graph";
 
 // Can reuse server instance with createServer
 // https://stackoverflow.com/questions/17696801/express-js-app-listen-vs-server-listen
-
-// GraphQL schema
-const schema = buildSchema(`
-    type Query {
-        message: String
-    }
-`);
-
-// Root resolver
-const root = {
-	message: () => "Hello World!"
-};
 
 const app = express();
 const dev = process.env.NODE_ENV !== "production";
@@ -25,14 +12,7 @@ app.get("/", (req, res) => {
 	res.send("Hello World! " + (dev ? "dev" : "prod"));
 });
 
-app.use(
-	"/graphql",
-	expressGraphQL({
-		schema: schema,
-		rootValue: root,
-		graphiql: true
-	})
-);
+app.use("/graphql", graph);
 
 createServer(app).listen(3574, () => {
 	console.log("Listening on port 3574!");
